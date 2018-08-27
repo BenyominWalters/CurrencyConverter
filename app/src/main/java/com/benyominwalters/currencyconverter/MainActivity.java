0d630d25ce8bf3ffecf7499bee5ee4f6a0e6dcf1
Benyomin Walters <31862759+BenyominWalters@users.noreply.github.com>
2018-08-27 12:27:23 -0500

    Conversion functionality with toast

--- app/src/main/java/com/benyominwalters/currencyconverter/MainActivity.java
+++ app/src/main/java/com/benyominwalters/currencyconverter/MainActivity.java
@@ -3,7 +3,23 @@ package com.benyominwalters.currencyconverter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
 public class MainActivity extends AppCompatActivity {
+
+    public void convert(View view) {
+
+        Log.i("Info:", "convert me!");
+
+        TextView amount = (TextView) findViewById(R.id.amountEditText);
+        Double convertedAmount = Double.valueOf(amount.getText().toString()) * 3.63;
+
+        Toast.makeText(MainActivity.this, convertedAmount.toString() + " Shekels", Toast.LENGTH_LONG).show();
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
